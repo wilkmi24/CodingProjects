1c6d52134d0c8e2e8773ddfe985734e7bfd7798f
wilkmi24 <70912258+wilkmi24@users.noreply.github.com>
2020-11-05 13:06:14 -0800

    uploading to GitHub

--- Python_Projects/DB_Assignmentp184/test.py
+++ Python_Projects/DB_Assignmentp184/test.py
@@ -1,42 +1,39 @@
 
 import sqlite3
 
-conn = sqlite3.connect('practice.db')
+conn = sqlite3.connect('trial.db')
 
 with conn:
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS tbl_files(\
         ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-        col_fname TEXT \
+        file_Name TEXT \
        )")
     conn.commit()
 conn.close()
 
 
-conn = sqlite3.connect('practice.db')
+conn = sqlite3.connect('trial.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES ('myMovie.mpg')"), \
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES ('information.docx')"), \
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES ('World.txt')"), \
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES ('myImage.png')"), \
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES ('data.pdf')"), \
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES ('Hello.txt')"), \
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES ('myPhoto.jpg')") \
+
 
     conn.commit()
-
+    
 conn.close()
 
 
 
-conn = sqlite3.connect('practice.db')
-
+conn = sqlite3.connect('trial.db')
 with conn:
     cur = conn.cursor()
-    cur.execute("SELECT * FROM tbl_files WHERE col_fname = '%txt'")
-    Results = cur.fetchall()
+    cur.execute("Select * FROM tbl_files WHERE file_Name = '%txt' ")
+    fileList = cur.fetchall()
+    for i in fileList:
+        files = 
 
-    
-    print(Results)
+
+fileList = ('information.docx','Hello.txt','myImage.png', \
+            'myMovie.mpg','World.txt','data.dpf','myPhoto.jpg')
+
