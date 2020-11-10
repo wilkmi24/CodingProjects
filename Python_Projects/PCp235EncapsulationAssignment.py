0dd5263631d882722c692fc632b57e9f649d6a95
wilkmi24 <70912258+wilkmi24@users.noreply.github.com>
2020-11-10 10:54:04 -0800

    uploading to GitHub

---
+++ Python_Projects/PCp235EncapsulationAssignment.py
@@ -0,0 +1,20 @@
+
+class vehicle:
+    body_style = 'unknown'
+    engine = 'unknown'
+
+class car(vehicle):
+    body_style = 'sudan'
+    engine = 'electric'
+    doors = 4
+    __drive = 'awd'
+
+class truck(vehicle):
+    body_style = 'double cab'
+    _engine = 'deisel'
+    color = 'blue-gray'
+    tires = 4
+
+    
+        
+        
